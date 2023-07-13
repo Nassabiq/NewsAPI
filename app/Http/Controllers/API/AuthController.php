45047302a1280526862192c1606e98ab14ed2739
Nassabiq <36199713+Nassabiq@users.noreply.github.com>
2023-07-14 00:16:37 +0700

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) return response(['errors' => $validator->errors()->all()], 422);
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('NewsAppAuthentication')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" => 'User does not exist'];
+            return response($response, 422);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $validator =  Validator::make($request->all(), [
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) return response(['errors' => $validator->errors()->all()], 422);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('NewsAppAuthentication')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+    //
+}
